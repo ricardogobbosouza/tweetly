a1ab3d3f792b5eb8076f4225e61226c12c0dbcaa
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2023-04-09 14:01:56 -0300

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vite'
 import unnoCSS from 'unocss/vite'
+import { presetUno } from 'unocss/preset-uno'
+import { presetIcons } from 'unocss/preset-icons'
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		unnoCSS()
+		unnoCSS({
+      presets: [
+        presetUno(),
+        presetIcons()
+      ]
+    })
 	],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
 });
